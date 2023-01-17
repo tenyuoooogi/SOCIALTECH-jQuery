7a6ae99085c31d480055e6f971adea22c95dd909
tenyuoooogi <tanjoubi0103@gmail.com>
2023-01-17 20:57:27 +0900

    カルーセル

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ $(function () {
       100
     );
   });
+
+  //カルーセル
+  $(".carousel").slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
 });
